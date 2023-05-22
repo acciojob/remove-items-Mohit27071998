b69b98e31c1a95b2d7b9cbe2f3b69969841893ae
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-22 11:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function deleteEle() {
+	let selele =document.getElementsByTagName("select")[0];
+	let selval = selele.value;
+	let childlist = selele.children;
+
+	for(let i= 0 ; i < childlist.length;i++){
+		if(selele === childlist[i].value){
+			childlist[i].remove();
+		}
+	}
+}
